df50385daf2e1f2f510a6dc914580caa0c9920c2
Gerardo Andres <gquispeh@uni.pe.com>
2021-12-17 07:40:30 -0500

    modulos para monolitic

---
+++ sale_order_line/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name' : 'Process Web Client',
+    'version': '1.0',
+    'depends' : ['base','sale'],
+    'data': [
+        'views.xml',
+    ],
+    'license': 'LGPL-3',
+}
---
+++ sale_subtask/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name' : 'Sale Subtask',
+    'version': '1.0',
+    'depends' : ['base','sale','project','sale_project'],
+    'data': [
+        'views.xml',
+    ],
+    'license': 'LGPL-3',
+}
