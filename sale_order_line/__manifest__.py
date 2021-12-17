5e8478d349347b2eae133e9d64154b477a9b6da6
Gerardo Andres <gquispeh@uni.pe.com>
2021-12-17 07:49:20 -0500

    nombre de modulos

--- sale_order_line/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    'name' : 'Process Web Client',
-    'version': '1.0',
-    'depends' : ['base','sale'],
-    'data': [
-        'views.xml',
-    ],
-    'license': 'LGPL-3',
-}
--- sale_subtask/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    'name' : 'Sale Subtask',
-    'version': '1.0',
-    'depends' : ['base','sale','project','sale_project'],
-    'data': [
-        'views.xml',
-    ],
-    'license': 'LGPL-3',
-}
